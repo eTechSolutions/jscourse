bac2d6eeac47bfe3f3959be3d8c232199f6494de
Robert Gill <envman@gmail.com>
2016-12-04 12:55:46 +0000

    added stuff (c1 tests)

---
+++ chapter-1/test.js
@@ -0,0 +1,11 @@
+var assert = require('assert')
+
+describe('Example Test File', function() {
+  it('Should Pass', function() {
+    assert(true)
+  })
+
+  it('Should Fail', function() {
+    assert(false, 'It Failed!')
+  })
+})
